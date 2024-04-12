2c12c1bd34e51139caef341bba9c9e29fbaa9625
kapee1 <volodya1907@gmail.com>
2024-04-13 03:48:15 +0600

    Удалил static и неявные ожидания из тестовых классов

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import pytest
 import settings
 from selenium import webdriver
+from locators import StellarBurgerLocators
+from data import StellarBurgerUserData
 
 
 @pytest.fixture(scope='function')
@@ -11,4 +13,9 @@ def driver():
     chrome_driver.quit()
 
 
-
+@pytest.fixture(scope='function')
+def login_from_login_page(driver):
+    driver.get(settings.login_url)
+    driver.find_element(*StellarBurgerLocators.EMAIL_FIELD).send_keys(StellarBurgerUserData.valid_user_email)
+    driver.find_element(*StellarBurgerLocators.PASSWORD_FIELD).send_keys(StellarBurgerUserData.valid_user_password)
+    driver.find_element(*StellarBurgerLocators.LOGIN_BUTTON_ON_LOGIN_PAGE).click()
